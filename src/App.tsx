d912f97cd2d60aa69c9e77b8c4909c4d498cd9f6
IvanCodeGit <email@email.com>
2024-02-12 23:43:59 +0200

    v1.41 (Library fix)

--- src/App.tsx
+++ src/App.tsx
@@ -326,11 +326,180 @@ const App: FC = () => {
         return { id, type, x1, y1, x2, y2, text: "", color };
       case "image":
         const img = new Image(x2 - x1, y2 - y1);
-        if (imageName) {
-          img.src = `./src/assets/library/${imageName}.png`;
-        } else {
-          img.src = `./src/assets/library/${selectedImage}.png`;
-          imageName = selectedImage;
+        switch (imageName) {
+          case "Vynychenko":
+            img.src = Vynychenko;
+            break;
+          case "Petlura":
+            img.src = Petlura;
+            break;
+          case "Bolbachan":
+            img.src = Bolbachan;
+            break;
+          case "Skoropadsky":
+            img.src = Skoropadsky;
+            break;
+          case "Nestor":
+            img.src = Nestor;
+            break;
+          case "Hryhorjev":
+            img.src = Hryhorjev;
+            break;
+          case "Denikin":
+            img.src = Denikin;
+            break;
+          case "Wrangel":
+            img.src = Wrangel;
+            break;
+          case "Vitovsky":
+            img.src = Vitovsky;
+            break;
+          case "Petrushevych":
+            img.src = Petrushevych;
+            break;
+          case "Kociubynskiy":
+            img.src = Kociubynskiy;
+            break;
+          case "Bezruczko":
+            img.src = Bezruczko;
+            break;
+          case "Konovalec":
+            img.src = Konovalec;
+            break;
+          case "RedFlag":
+            img.src = RedFlag;
+            break;
+          case "Swords":
+            img.src = Swords;
+            break;
+          case "Castle":
+            img.src = Castle;
+            break;
+          case "UkraineFlag":
+            img.src = UkraineFlag;
+            break;
+          case "WhiteFlag":
+            img.src = WhiteFlag;
+            break;
+          case "SovietFlag":
+            img.src = SovietFlag;
+            break;
+          case "AktZluky":
+            img.src = AktZluky;
+            break;
+          case "PetluraPislydski":
+            img.src = PetluraPislydski;
+            break;
+          case "GermanEmpireFlag":
+            img.src = GermanEmpireFlag;
+            break;
+          case "AustroHungaryFlag":
+            img.src = AustroHungaryFlag;
+            break;
+          case "RomaniaFlag":
+            img.src = RomaniaFlag;
+            break;
+          default:
+            switch (selectedImage) {
+              case "Vynychenko":
+                img.src = Vynychenko;
+                imageName = "Vynychenko";
+                break;
+              case "Petlura":
+                img.src = Petlura;
+                imageName = "Petlura";
+                break;
+              case "Bolbachan":
+                img.src = Bolbachan;
+                imageName = "Bolbachan";
+                break;
+              case "Skoropadsky":
+                img.src = Skoropadsky;
+                imageName = "Skoropadsky";
+                break;
+              case "Nestor":
+                img.src = Nestor;
+                imageName = "Nestor";
+                break;
+              case "Hryhorjev":
+                img.src = Hryhorjev;
+                imageName = "Hryhorjev";
+                break;
+              case "Denikin":
+                img.src = Denikin;
+                imageName = "Denikin";
+                break;
+              case "Wrangel":
+                img.src = Wrangel;
+                imageName = "Wrangel";
+                break;
+              case "Vitovsky":
+                img.src = Vitovsky;
+                imageName = "Vitovsky";
+                break;
+              case "Petrushevych":
+                img.src = Petrushevych;
+                imageName = "Petrushevych";
+                break;
+              case "Kociubynskiy":
+                img.src = Kociubynskiy;
+                imageName = "Kociubynskiy";
+                break;
+              case "Bezruczko":
+                img.src = Bezruczko;
+                imageName = "Bezruczko";
+                break;
+              case "Konovalec":
+                img.src = Konovalec;
+                imageName = "Konovalec";
+                break;
+              case "RedFlag":
+                img.src = RedFlag;
+                imageName = "RedFlag";
+                break;
+              case "Swords":
+                img.src = Swords;
+                imageName = "Swords";
+                break;
+              case "Castle":
+                img.src = Castle;
+                imageName = "Castle";
+                break;
+              case "UkraineFlag":
+                img.src = UkraineFlag;
+                imageName = "UkraineFlag";
+                break;
+              case "WhiteFlag":
+                img.src = WhiteFlag;
+                imageName = "WhiteFlag";
+                break;
+              case "SovietFlag":
+                img.src = SovietFlag;
+                imageName = "SovietFlag";
+                break;
+              case "AktZluky":
+                img.src = AktZluky;
+                imageName = "AktZluky";
+                break;
+              case "PetluraPislydski":
+                img.src = PetluraPislydski;
+                imageName = "PetluraPislydski";
+                break;
+              case "GermanEmpireFlag":
+                img.src = GermanEmpireFlag;
+                imageName = "GermanEmpireFlag";
+                break;
+              case "AustroHungaryFlag":
+                img.src = AustroHungaryFlag;
+                imageName = "GermanEmpireFlag";
+                break;
+              case "RomaniaFlag":
+                img.src = RomaniaFlag;
+                imageName = "GermanEmpireFlag";
+                break;
+              default:
+                throw new Error(`Type no recognised: ${type}`);
+            }
         }
         return { id, type, x1, y1, x2, y2, img, imageName };
     }
